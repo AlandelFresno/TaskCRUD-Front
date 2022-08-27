1940a893de4ae35a64e2ebc05df19eb24834ef37
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-27 00:42:47 -0300

    🚧 PROGRESS:  Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,31 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
+import Link from 'next/link';
+import styles from './styles/Navbar.module.scss';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <div className={styles.NavbarContainer}>
+      <nav className={styles.menuToggle}>
+        <input type="checkbox" onClick={handleClick} />
+        {isClicked ? (
+          <ClearIcon className={styles.menuIcon2} />
+        ) : (
+          <MenuIcon className={styles.menuIcon1} />
+        )}
+        <ul>
+          <Link href="#">Home</Link>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
