8f6aead54f181e83d39bb06a8ea338e729cc6655
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-27 23:09:25 -0300

    🎨 IMPROVE:  Redux base implementation
    Added user name in the navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import ClearIcon from '@mui/icons-material/Clear';
 import Link from 'next/link';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './styles/Navbar.module.scss';
 import { useState } from 'react';
+import { Typography } from '@mui/material';
+import { RootState } from '../../store';
 
 const Navbar = () => {
   const [isClicked, setIsClicked] = useState(false);
-
+  const data = useSelector((state: RootState) => state.auth);
+  const { user, error, loading, token } = data;
   const handleClick = () => {
     setIsClicked(!isClicked);
   };
@@ -22,6 +26,13 @@ const Navbar = () => {
         )}
         <ul>
           <Link href="/home">Home</Link>
+          {user.name ? (
+            <Typography variant="body1" className={styles.userName}>
+              {user.name}
+            </Typography>
+          ) : (
+            <></>
+          )}
         </ul>
       </nav>
     </div>
