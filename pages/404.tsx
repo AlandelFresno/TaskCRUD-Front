2a965b4ba0dbde0f1690553e05156f6523f5530b
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-27 23:05:19 -0300

    Navbar in 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import { Typography } from '@mui/material';
+import Navbar from '../components/Navbar/Navbar';
 import styles from './styles/Custom404.module.scss';
 
 const Custom404 = () => {
   return (
     <div className={styles.main}>
+      <Navbar />
       <Typography variant="h2">404</Typography>
       <Typography variant="subtitle1">Page not found</Typography>
     </div>
