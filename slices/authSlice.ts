6e88fe7e1baf5447ffd7c036c80f6e13b3971b9c
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-29 09:53:48 -0300

    ✨ NEW FEATURE: Redux actions

--- slices/authSlice.ts
+++ slices/authSlice.ts
@@ -5,14 +5,14 @@ import { RootState } from '../store';
 export interface AuthState {
   loading: boolean;
   error: string | null;
-  user: {name: string, email: string, password: string};
-  token: string | null;
+  user: { name: string; email: string; password: string };
+  token: string;
 }
 
 const initialState: AuthState = {
   loading: true,
   error: null,
-  user: {name: '', email: '', password: ''},
+  user: { name: '', email: '', password: '' },
   token: '',
 };
 
@@ -20,15 +20,24 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state) => {
+    login: (state, action: PayloadAction<object>) => {
+      state.user = { ...state.user, ...action.payload };
+    },
+    logout: (state) => {
+      state.user = { ...initialState.user };
+    },
+    saveToken: (state, action: { payload: string }) => {
+      state.token = action.payload;
       return state;
+    },
+    removeToken: (state) => {
+      state.token = initialState.token;
     },
   },
 });
+export default authSlice.reducer;
 
-export const { login } = authSlice.actions;
+export const { login, logout, saveToken, removeToken } = authSlice.actions;
 
 // Selectors
 export const selectorAuth = (state: RootState) => state.auth;
-
-export default authSlice.reducer;
