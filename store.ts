108a9c48f8f770f2beaba9c4a23f57e0aafb4ac1
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-29 10:03:40 -0300

    🎨 IMPROVE: Redux store

--- store.ts
+++ store.ts
@@ -1,5 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/AuthSlice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import {useDispatch} from 'react-redux';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
@@ -11,3 +12,11 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch 
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
