96f0b6c78066c6cf19cdc95ef2abdbb946d1250a
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-29 09:57:42 -0300

    💄UI: Navbar login/logout button

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,13 +6,26 @@ import styles from './styles/Navbar.module.scss';
 import { useState } from 'react';
 import { Typography } from '@mui/material';
 import { RootState } from '../../store';
+import { useRouter } from 'next/router';
+import { logoutHelper } from '../../helpers/authHelper';
 
 const Navbar = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
   const [isClicked, setIsClicked] = useState(false);
   const data = useSelector((state: RootState) => state.auth);
   const { user, error, loading, token } = data;
   const handleClick = () => {
     setIsClicked(!isClicked);
+  };
+
+  const handleLoginClick = () => {
+    console.log(router)
+    router.push('/login');
+  };
+
+  const handleLogoutClick = () => {
+    logoutHelper(dispatch, router);
   };
 
   return (
@@ -26,6 +39,11 @@ const Navbar = () => {
         )}
         <ul>
           <Link href="/home">Home</Link>
+          {!token ? (
+            <button onClick={handleLoginClick}>Log in</button>
+          ) : (
+            <button onClick={handleLogoutClick}>Log out</button>
+          )}
           {user.name ? (
             <Typography variant="body1" className={styles.userName}>
               {user.name}
