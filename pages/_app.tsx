f75f559cb6fcd9dfa0a79e622121716fa2937710
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-29 10:01:42 -0300

    ✨ NEW FEATURE: auth/me

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../globalStyle/globals.css';
 import type { AppProps } from 'next/app';
 import { store } from '../store';
 import { Provider } from 'react-redux';
+import { AuthProvider } from '../hooks/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </Provider>
   );
 }
