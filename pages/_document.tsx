6d07ec176616f7559238efea9f86b735bdd948e8
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-29 22:54:35 -0300

    🐛 BUG: No title
    Removed unnecessary head in index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ const _document = () => {
   return (
     <Html>
       <Head>
+        <title>Trackmania</title>
+        <meta name="description" content="Technical test" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
